5dc9c5229fc2d25b54cf841aa3fe7fd1373f95aa
Anderson Pedroza <anderson.pedroza@uniminuto.edu.co>
2022-10-12 18:54:04 -0500

    se agregan variables de deploy

--- Proyecto_Ferreteria/src/main/java/connection/DBConnection.java
+++ Proyecto_Ferreteria/src/main/java/connection/DBConnection.java
@@ -6,17 +6,23 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     Connection connection;
-    static String db = "ferreteria";
-    static String port = "3306";   /* El puerto de la base de datos 3307 */
+//    static String db = "ferreteria";
+//    static String port = "3306";   /* El puerto de la base de datos 3307 */
+//    static String login = "root";
+//    static String password = "admin";
+    
+    static String db = "railway";
+    static String port =  "6751";
     static String login = "root";
-    static String password = "admin";
-
+    static String password = "uB8whzaQ5pHr4GRDa7gZ";   
+    static String ip = "containers-us-west-89.railway.app"; 
+    
     public DBConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:"+this.port+ "/"+this.db;
+            String url = "jdbc:mysql://"+DBConnection.ip +":"+DBConnection.port+ "/"+DBConnection.db;
             
-            connection = DriverManager.getConnection(url, this.login, this.password);
+            connection = DriverManager.getConnection(url, DBConnection.login, DBConnection.password);
             System.out.println("Conexion exitosa");
         } catch (Exception ex) {
             System.out.println("Error en la conexion " + ex);
